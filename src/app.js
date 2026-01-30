8e1278754f6fe230be974f025d4405d2b58c9e68
Mus'ab <oghayidflash1008@gmail.com>
2026-01-30 19:45:53 +0100

    app: route declaration

--- src/app.js
+++ src/app.js
@@ -12,7 +12,12 @@ const app = express();
 
 app.use (express.json());
 
+//Routes declaration
 app.use('/api/auth', authRoutes);
+
+
+//example route: http://localhost:5000/api/auth/register
+
 
 app.get('/', (req, res) => {
     res.send('Welcome to AuthGuard API');
